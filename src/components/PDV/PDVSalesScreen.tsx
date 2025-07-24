7058878bf66e1519ceee700703611bf4145ae3b8
gleilsonbarbosa2 <gleilsonbarbosa1@gmail.com>
2025-07-24 19:49:15 -0300

    Updated PDVSalesScreen.tsx

--- src/components/PDV/PDVSalesScreen.tsx
+++ src/components/PDV/PDVSalesScreen.tsx
@@ -725,7 +725,7 @@ const Store2CashRegisterMenu: React.FC = () => {
           summary={summary}
           entries={entries}
           onClose={() => setShowPrintView(false)}
-        />
+       
       )}
     </div>
   );
